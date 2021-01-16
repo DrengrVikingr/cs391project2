5d57ba47d6f714906d2365f32791aae4148a9a72
Hasan <hasan.bilgin@ozu.edu.tr>
2021-01-17 00:11:45 +0300

    fixed a problem about navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import Logo from '../assets/logo-cream.png';
 import './NavBar-Footer.css';
-import Navbar from 'react-bootstrap/Navbar'
-import { Link } from 'react-router-dom'
+import { Navbar, Nav } from 'react-bootstrap'
 
 class NavBar extends Component { 
   
   state = {}
   render() {
-    return (<Navbar bg="secondary" >
+    return (
+    <Navbar bg="secondary" >
       <Navbar.Brand href="#home">
         <img
           src={Logo}
@@ -20,20 +20,12 @@ class NavBar extends Component {
         <a class="marmotte" href="/" style={{color:'white'}}>La Marmotte</a>
       </Navbar.Brand>
       <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-        <ul class="navbar-nav"  >
-          <li class="nav-item"  >
-            <Link to="/" style={{ color: "white", fontSize: 18, marginRight: '20px', textDecoration: 'none'}}>Home</Link>
-          </li>
-          <li class="nav-item">
-            <Link to="/categories" style={{ color: "white", fontSize: 18, marginRight: '20px', textDecoration: 'none' }}>Categories</Link>
-          </li>
-          <li class="nav-item">
-            <Link to="/add_recipe" style={{ color: "white", fontSize: 18, marginRight: '20px', textDecoration: 'none' }}>Add Recipe</Link>
-          </li>
-        </ul>
-
+        <Nav>
+            <Nav.Link href="/" style={{ color: "white", fontSize: 18, marginRight: '20px', textDecoration: 'none'}}>Home</Nav.Link>
+            <Nav.Link href="/categories" style={{ color: "white", fontSize: 18, marginRight: '20px', textDecoration: 'none' }}>Categories</Nav.Link>
+            <Nav.Link href="/add_recipe" style={{ color: "white", fontSize: 18, marginRight: '20px', textDecoration: 'none' }}>Add Recipe</Nav.Link>
+        </Nav>
       </div>
-
     </Navbar>);
   }
 }
