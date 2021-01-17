d3485cb31da5d65519b47bc546da7929841d02d1
waterfairyliterate <waterfairyliterate@users.noreply.github.com>
2021-01-17 22:53:48 +0300

    recipe files updated

--- src/components/recipe_info/Falafel.jsx
+++ src/components/recipe_info/Falafel.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Footer from '../Footer';
 import NavBar from '../NavBar';
 import Recipe from '../Recipe';
+import { recipes } from "../data";
+
 
 class Falafel extends Component {
     state = {}
@@ -15,54 +17,39 @@ class Falafel extends Component {
         );
     }
 
-    getName(name) {
-        return "Falafel";
-    }
     getTitle(name) {
-        return "Falafel";
+        return recipes.map((data) => {
+            if (data.name === name)
+                return data.title;
+        });
     }
 
+
     getAuthor(name) {
-        return "Lisa Bryan";
+        return recipes.map((data) => {
+            if (data.name === name)
+                return data.author;
+        });
     }
 
     getDesc(name) {
-        return "Falafel are delicious balls of chickpea and herb goodness that you find in Middle Eastern cooking. Naturally vegan and vegetarian, falafel are great in wraps, pitas, sandwiches and salads. Today, I’ll share how to make both fried falafel and baked falafel. You choose your favorite! "
-        + "Are you ready for the best falafel you’ve ever tasted? I’ve had my fair share of falafel on travels through Egypt, Israel and Jordan. " 
-        + "I’ve had them in restaurants and on street corners (about as authentic as they come). I’ve had them stuffed in gluten-free pita and on salads. And I’ve had them with slight variations and tweaks, though the recipe itself is fairly simple. "
-        + "But after eating my way through Tel Aviv last year, I realised what makes the best falafel. And here’s the answer: tons of herbs (double the normal amount) and a small amount of green pepper. "
-        + "This makes for an addictive flavor that’s “a little something extra” but not spicy. Just insanely delicious.Traditional falafel is made from ground chickpeas or fava beans, herbs and spices. "
-        + "The mixture is then formed into balls or patties and deep fried for a texture that’s crispy on the outside and soft on the inside, sort of like a fritter. It’s a classic Middle Eastern and Mediterranean recipe that’s enjoyed as street food and often served up as part of mezze (a group of small meals). ";
-}
+        return recipes.map((data) => {
+            if (data.name === name)
+                return data.description;
+        });}
 
     getIngredients(name) {
-        return "1 cup dried chickpeas, soaked overnight (don't use canned chickpeas)\n"
-        + "1/2 cup onion, roughly chopped\n"
-        + "1 cup parsley, roughly chopped (about a one large bunch)\n"
-        + "1 cup cilantro, roughly chopped (about a one large bunch)\n"
-        + "1 small green chile pepper, serrano or jalapeno pepper\n"
-        + "3 garlic cloves\n"
-        + "1 tsp cumin\n"
-        + "1 tsp salt\n"
-        + "1/2 tsp cardamom\n"
-        + "1/4 tsp black pepper\n"
-        + "2 tbsp chickpea flour\n"
-        + "1/2 tsp baking soda\n"
-        + "avocado oil for frying";
+        return recipes.map((data) => {
+            if (data.name === name)
+                return data.ingredients;
+        });
     }
 
     getInstructions(name) {
-        return "1.	The night before, soak the dried chickpeas in water. Make sure the water covers the chickpeas by 2-3 inches, as they'll triple in size. Dried chickpeas soaking in a bowl of water.\n"
-        + "2.	Drain and rinse the chickpeas and add them to your food processor, along with the onion, parsley, cilantro, pepper, garlic, cumin, salt, cardamom and black pepper.\n"
-        + "3.	Pulse the food processor several times until the mixture resembles the texture of coarse sand.\n"
-        + "4.	Transfer the falafel mixture to a bowl and add the chickpea flour and baking soda. Stir together, then cover or add a lid and refrigerate the mixture for 30 minutes to one hour. Falafel ingredients in a mixing bowl.\n"
-        + "5.	Use your hands, an ice cream scoop or falafel scoop to form the falafel into balls or patties. If you find the mixture is too wet, you can add another tablespoon of chickpea flour. If it's too dry and crumbly, you can add a teaspoon or two of water or lemon juice. Scooping balls of falafel from the raw mixture.\n"
-        + "6.	Once the falafel are formed, you can cook them by your preferred method mentioned above. To deep fry the falafel, add about 3 inches of oil to a pot on medium heat. Heat the oil to 350F. Cook the falafel in batches (about 6-8 at a time) for 1-2 minutes or until golden.\n"
-        + "7.	Cooking the falafel in a pan of oil on the stove.\n"
-        + "Use a skimmer to check the color of the falafel and make sure they don't over cook. Then remove them to a paper towel-lined plate.\n"
-        + "Removing the falafel from the pan once they're cooked and golden.\n"
-        + "Serve the falafel immediately, while warm and crispy on the outside. They're delicious served with tahini sauce as well.\n";}
-
+        return recipes.map((data) => {
+            if (data.name === name)
+                return data.instructions;
+        });}
 }
 
 export default Falafel;
--- src/components/recipe_info/TuscanChicken.jsx
+++ src/components/recipe_info/TuscanChicken.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Footer from '../Footer';
 import NavBar from '../NavBar';
 import Recipe from '../Recipe';
+import { recipes } from "../data";
+
 
 class TuscanChicken extends Component {
     state = {}
@@ -15,50 +17,39 @@ class TuscanChicken extends Component {
         );
     }
 
-    getName(name) {
-        return "Tuscan Chicken";
-    }
     getTitle(name) {
-        return "Stuffed Tuscan Chicken";
+        return recipes.map((data) => {
+            if (data.name === name)
+                return data.title;
+        });
     }
 
+
     getAuthor(name) {
-        return "Jessica Gavin";
+        return recipes.map((data) => {
+            if (data.name === name)
+                return data.author;
+        });
     }
 
     getDesc(name) {
-        return "It’s convenient to grab a pack of chicken breasts from the market, but the big question is how to turn it into something special. To spice things, try stuffing them with a savory vegetable and cheese mixture. As the protein cooks, the filling gets hot and bubbly, infusing the aromatic garlic and herb flavors into each piece.\n"
-        + "The chicken cooks in one skillet to make preparation effortless. Once stuffed, the meat sears until golden on each side, then finishes in the oven for gentle cooking. A delicious low carb and keto-friendly entree to feed a family or reheat for meal prep. Pair with your favorite side dishes like crispy green beans or tender broccoli for a complete meal.\n"
-        + "The stuffing is easy to prepare. Just chop and mix! Softened cream cheese is the base, binding the ingredients together and providing a creamy, tangy taste. For a Tuscan-inspired flavor, I looked to fresh greens, herbs, and sundried tomatoes. Fresh spinach, basil, parsley, chives, and garlic provide herbaceous and earthy notes. \n"
-        + "The tomatoes concentrate their sweetness during baking and prevent the filling from getting watery as it cooks. Parmesan cheese adds a nutty taste, and gooey melt when sliced. For a quick pantry swap, 1 tablespoon of dried Italian seasoning can be substituted for the herbs.\n"
-        + "I recommend using chicken breasts that are about 6 to 8 ounces. This size provides a thick enough piece to slice and fill. The smaller size will cook quicker, so you may need to reduce the time in the oven slightly.;\n"
-        +"A pocket to hold the filling needs to be cut into the thickest side of the meat. Start by placing the breast on a cutting board and grabbing a sharp utility or paring knife. Hold the knife parallel to the board and cut a slit through the chicken’s side down the middle, about 5 inches.\n"
-        + "Be sure not to cut all the way through. You want to make sure there are no holes in the pocket. Otherwise, the stuffing will ooze out. I find it easiest to season the surface with salt and pepper before adding the filling.";
-}
+        return recipes.map((data) => {
+            if (data.name === name)
+                return data.description;
+        });}
 
     getIngredients(name) {
-        return "4 boneless skinless chicken breast, about 2 ½ to 3 pounds total\n"
-        + "1 ½ teaspoon kosher salt, divided\n"
-        + "¾ teaspoon black pepper, divided\n"
-        + "4 ounces cream cheese, softened 60 to 65ºF (15 to 18ºC)\n"
-        + "½ cup grated parmesan cheese\n"
-        + "2 cups baby spinach, loosely packed, finely chopped\n"
-        + "¼ cup sun-dried tomatoes, in oil, chopped\n"
-        + "1 tablespoon chopped basil\n"
-        + "1 tablespoon chopped parsley"
-        + "1 tablespoon sliced chives, or green onions\n"
-        + "1 teaspoon minced garlic, or ½ teaspoon garlic powder\n";
+        return recipes.map((data) => {
+            if (data.name === name)
+                return data.ingredients;
+        });
     }
 
     getInstructions(name) {
-        return "1.	Set the oven rack to the lower-middle position. Preheat to 375ºF (191ºC).\n"
-        + "2.	On the thickest side of the chicken breast, cut about a 4 to 5-inch long slit. Continue to cut a 2 to 3-inch deep pocket, leaving about a 1-inch uncut area to keep the other side connected. Make sure not to cut through the bottom. Repeat with the remaining pieces.\n"
-        + "3.	In a small bowl, combine 1 teaspoon salt and ½ teaspoon black pepper. Evenly season each side of the chicken with the mixture.\n"
-        + "4.	In a medium bowl, combine softened cream cheese, parmesan cheese, spinach, sun-dried tomato, basil, parsley, chives, garlic, ½ teaspoon salt, and ¼ teaspoon black pepper.\n"
-        + "5.	Evenly spread the cheese mixture into the pocket of each chicken breast, about 2 to 4 tablespoons, do not overfill. If desired, close the opening with toothpicks.\n"
-        + "6.	Heat a large oven-proof skillet or Dutch oven over medium heat. Once the pan is hot, add the olive oil.\n"
-        + "7.	Add the chicken, presentation side up, lightly press the meat down to make contact with the pan. Cook until golden brown on the surface, about 5 to 7 minutes. Flip over and cook for 5 minutes. If using smaller chicken breasts, check for doneness as baking may not be needed.\n"
-        + "8.	Transfer the pan to the oven. Bake until the meat is no longer pink and the internal temperature reaches 160 to 165ºF (71 to 74ºC), about 8 to 12 minutes depending on the thickness of the meat. Rest the chicken for 5 minutes, then serve hot.\n";}
+        return recipes.map((data) => {
+            if (data.name === name)
+                return data.instructions;
+        });}
 }
 
 export default TuscanChicken;
